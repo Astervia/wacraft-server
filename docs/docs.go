45cbf0b67b80fa54e1f3fa6a0dad36282e328900
Ruy Vieira <ruy.vieiraneto@gmail.com>
2026-04-09 20:32:45 -0300

    chore(docs): update version OpenAPI

--- docs/docs.go
+++ docs/docs.go
@@ -15158,7 +15158,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.2.0",
+	Version:          "0.2.10",
 	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
