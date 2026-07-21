4201ce931a1d8bc54c4d0ffdd61f7cc4af7a1c5c
Ruy Vieira <ruy.vieiraneto@gmail.com>
2026-07-21 14:10:39 -0300

    chore(release): `v0.2.16`

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 // @title						wacraft Server API
-// @version					0.2.15
+// @version					0.2.16
 // @description				Backend server for the wacraft project. Handles WhatsApp Cloud API operations, including message sending, receiving, and webhook handling.
 // @contact.name				Astervia Dev Team
 // @contact.url				https://github.com/Astervia
